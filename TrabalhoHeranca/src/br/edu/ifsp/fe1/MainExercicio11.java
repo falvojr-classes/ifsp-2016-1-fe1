16d0cbb16c86489110a455e0cd8612e3f312c90b
Venilton FalvoJr <falvojr@gmail.com>
2016-05-30 20:55:25 -0300

    Exercicios 30/05/2016

--- TrabalhoHeranca/src/br/edu/ifsp/fe1/MainExercicio11.java
+++ TrabalhoHeranca/src/br/edu/ifsp/fe1/MainExercicio11.java
@@ -2,6 +2,7 @@ package br.edu.ifsp.fe1;
 
 import br.edu.ifsp.fe1.entidades.Clube;
 import br.edu.ifsp.fe1.entidades.Jogador;
+import br.edu.ifsp.fe1.entidades.PreparadorFisico;
 import br.edu.ifsp.fe1.entidades.Tecnico;
 
 /**
@@ -21,6 +22,8 @@ public class MainExercicio11 {
         Jogador jogador3 = new Jogador(100, clube);
         Jogador jogador4 = new Jogador(50, clube);
         Jogador jogador5 = new Jogador(500, clube);
+        PreparadorFisico preparador = new PreparadorFisico(39.5, clube);
+        preparador.setEspecialidade(PreparadorFisico.RESISTENCIA);
         
         tecnico.receberSalario();
         jogador1.receberSalario();
@@ -28,6 +31,7 @@ public class MainExercicio11 {
         jogador3.receberSalario();
         jogador4.receberSalario();
         jogador5.receberSalario();
+        preparador.receberSalario();
         
         System.out.println(tecnico.isFeliz());
         System.out.println(jogador1.isFeliz());
@@ -35,6 +39,7 @@ public class MainExercicio11 {
         System.out.println(jogador3.isFeliz());
         System.out.println(jogador4.isFeliz());
         System.out.println(jogador5.isFeliz());
+        System.out.println(preparador.isFeliz());
         System.out.println(clube.getSaldo());
     }
 }
